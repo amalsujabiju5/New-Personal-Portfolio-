facb9a81c4c6743234001d33a3a7934fa6c76d41
Amal <99679990+amalsujabiju5@users.noreply.github.com>
2024-06-23 14:02:08 -0400

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -25,11 +25,11 @@ export const assets = {
 export const intro = [
     {
         title: "Amal",
-        desc: "This is my personal portfolio website. You can see my Skills, Experience, Projects, Education, and Certifications on this website. Feel free to play around with my website.",
+        desc: "This is my personal portfolio website and in this website you can see my Skills,Experience,Projects,Education and Certifications. Feel Free to Play around with my Website",
     },
     {
         title: "Introduction",
-        desc: "Amal is a seasoned computer science and software development professional with a college education and experience. With a solid academic foundation in computer science and a passion for problem-solving, Amal is committed to delivering top-tier solutions to intricate challenges. His portfolio showcases a diverse range of projects and accomplishments, highlighting his dedication to excellence in every endeavor.",
+        desc: "Amal is a seasoned computer science and software development professional, bringing college education and experience With a solid academic foundation in computer science and a passion forproblem-solving,Amal is committed to delivering top-tier solutions to intricate challenges. His portfolio showcases a diverse range of projects and accomplishments, highlighting his dedication to excellence in every endeavor",
     },
 ];
 
@@ -40,29 +40,29 @@ export const education = [
     },
     {
         company_name: "GVHSS Pattazhy India",
-        desc: "Completed high school education from GVHSS in Bio-Maths stream and scored 81% overall.",
+        desc: "Completed High school education from GVHSS in Bio-Maths stream and scored 81% Overall ",
     },
     {
         company_name: "Google Technical Support",
-        desc: "Completed Google Technical Support Fundamentals certificate, which helped in understanding technical support fundamentals.",
+        desc: "Completed Google Technical support fundementals certificate which helped in understanding of technical support fundementals ",
     },
 ];
 
 export const technologies = [
     {
-        name: "Software Developer",
+        name: "Software developer",
         icon: software,
     },
     {
-        name: "Web Developer",
+        name: "Web developer",
         icon: web,
     },
     {
-        name: "Frontend Developer",
+        name: "Frontend Devloper",
         icon: front,
     },
     {
-        name: "Flutter Developer",
+        name: "Flutter Devloper",
         icon: mobile,
     },
 ];
@@ -70,32 +70,43 @@ export const technologies = [
 export const projects = [
     {
         name: "YouTube Clone",
-        description: "YouTube clone is an application that allows you to watch your favorite videos and contents and search videos and shows.",
+        description:
+            "Youtube clone is an application that allows you to watch your favourite videos and contents and search videos and shows.",
+
         image: youtube,
     },
     {
         name: "Spotify Clone",
-        description: "Spotify clone is an application that allows you to stream your favorite music and create playlists.",
+        description:
+            "Spotify clone is an application that allows you to stream your favourite music and create playlists.",
+
         image: spotify,
     },
     {
         name: "Netflix Clone",
-        description: "Netflix clone is an application made with Flutter. It features previewing movies and detailed information fetched from the TMDB database.",
+        description:
+            "Netflix clone is an application that is made up of flutter. It has the feature of previewing the movies and when the user clicks the Movies list then it will show the detail that coming from TMDB database.",
+
         image: netflix,
     },
     {
-        name: "Personal Portfolio",
-        description: "Personal Portfolio project showcasing my experience, skills, projects, and contact information. This project is made with React.",
+        name: "Personal poertfolio",
+        description:
+            "Personal Portfolio prject showcasing my experience,skills,Projects and Contact infrormation.This project is made with React",
+
         image: portfolio,
     },
     {
         name: "Weather App",
-        description: "Weather app has features like location search, current weather display, and forecast.",
+        description:
+            "Weather app shows has the feature of searching the location and the user can able to see the current weather of the Particular place and you can see the Forecast of the place in the same age als0.",
+
         image: weather,
     },
     {
         name: "ChatApp",
-        description: "ChatApp using React is an online chat platform where users can chat with registered users. Users need to register first, fill in their details, and select a profile picture. It includes authentication features for user login and logout.",
+        description:
+            "ChatApp using react is an online chat Platform. where the user can chat to the registered Users.The user has to register first with filling the detaials and selecting a profile picture,It has aunthentication features which Login and logout the users. ",
         image: ChatApp,
     },
 ];
